728e9ecc01873ca7bdc7523be01c7ae8e5326ba0
Jason Cho <jason.cho.86@gmail.com>
2019-04-22 12:39:49 -0400

    Moved pwd to pwd.js

--- bash.js
+++ bash.js
@@ -2,14 +2,6 @@
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
-process.stdin.on('data', data => {
-  const cmd = data.toString().trim(); // remove the new line
+const { pwdCmd } = require('./pwd');
 
-  if (cmd.toLowerCase() === 'pwd') {
-    process.stdout.write(process.cwd());
-  } else {
-    process.stdout.write('You typed: ' + cmd);
-  }
-
-  process.stdout.write('\nprompt > ');
-});
+pwdCmd();
