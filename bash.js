a3cd7a448b7012892be706587c505a0e967b3cfb
Jason Cho <jason.cho.86@gmail.com>
2019-04-22 12:27:05 -0400

    added pwd functionality

--- bash.js
+++ bash.js
@@ -1,10 +1,15 @@
-// OUtput a prompt
+// Output a prompt
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', data => {
   const cmd = data.toString().trim(); // remove the new line
 
-  process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt >');
+  if (cmd.toLowerCase() === 'pwd') {
+    process.stdout.write(process.cwd());
+  } else {
+    process.stdout.write('You typed: ' + cmd);
+  }
+
+  process.stdout.write('\nprompt > ');
 });
